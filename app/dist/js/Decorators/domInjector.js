3de1025dc534954636f0bd93fa5081ed25c23c69
valeriaFaccin <valeriafaccin00@gmail.com>
2024-06-19 16:10:18 -0300

    adicionando verificação de existência do elemento no domInjector

--- app/dist/js/Decorators/domInjector.js
+++ app/dist/js/Decorators/domInjector.js
@@ -1,9 +1,12 @@
 export function domInjector(selector) {
     return function (target, propertyKey) {
         console.log(`Modificando prototype ${target.constructor.name}, e adicionando getter para a propriedade ${propertyKey}`);
+        let element;
         const abobrinha = function () {
-            const element = document.querySelector(selector);
-            console.log(`Buscando elementos do DOM com o seletor ${selector} para injetar em ${propertyKey}`);
+            if (!element) {
+                element = document.querySelector(selector);
+                console.log(`Buscando elementos do DOM com o seletor ${selector} para injetar em ${propertyKey}`);
+            }
             return element;
         };
         Object.defineProperty(target, propertyKey, { get: abobrinha });
--- app/src/Decorators/domInjector.ts
+++ app/src/Decorators/domInjector.ts
@@ -4,10 +4,16 @@ export function domInjector(selector: string) {
         propertyKey: string
     ) {
         console.log(`Modificando prototype ${target.constructor.name}, e adicionando getter para a propriedade ${propertyKey}`);
-        //metodo get para o elemento HTML - método com nome de abobrinha para provar que o nome não necessariamente importa para a funcionalidade get dos elementos
+
+        //método get para o elemento HTML 
+        let element: HTMLElement;
         const abobrinha = function () {
-            const element = document.querySelector(selector);
-            console.log(`Buscando elementos do DOM com o seletor ${selector} para injetar em ${propertyKey}`);
+            //verifica se o elemento buscado já existe ou não
+            if(!element) {
+                element = <HTMLElement>document.querySelector(selector);
+                console.log(`Buscando elementos do DOM com o seletor ${selector} para injetar em ${propertyKey}`);
+            }
+            
             return element;
         }
 
